cddfe644feee3e11d0cba3e4bc08a3948ec6ef17
feihtthief <feihtthief@gmail.com>
2015-09-14 07:38:35 +0200

    More API Calls implemented

---
+++ FW.BitX/Entities/Local/TickerListEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FW.BitX.Entities.Local
+{
+	// todo: combine this with with TickerInfo ???
+	// maybe make this be the result, and automatically add the PAIR from the ticker?pair=Pair call???
+	public class TickerListEntry : TickerInfo
+	{
+		//public decimal Ask { get; set; }
+		//public long BitXTimeStamp { get; set; }
+		//public DateTime TimeStampUTC { get; set; }
+		//public decimal Bid { get; set; }
+		//public decimal Rolling24HourVolume { get; set; }
+		//public decimal LastTrade { get; set; }
+		public string Pair { get; set; }
+	}
+}
---
+++ FW.BitX/Entities/Remote/BitX_AllTickers_QueryResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FW.BitX.Entities.Remote
+{
+	// https://api.mybitx.com/api/1/tickers
+
+	public class BitX_AllTickers_QueryResponse
+	{
+		public BitX_AllTickers_TickerEntry[] tickers { get; set; }
+	}
+
+
+
+}
